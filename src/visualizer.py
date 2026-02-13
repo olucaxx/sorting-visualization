7a664ae8a26ac5d6634d1272bfa516033bc9ac29
olucaxx <devpereiralucas@gmail.com>
2026-02-13 10:54:56 -0300

    updates with steps, not frames

--- src/visualizer.py
+++ src/visualizer.py
@@ -6,6 +6,7 @@ from state import Array
 SIZE = 100
 SCALE = 5
 FPS = 60
+STEP = 1/100
 
 # init 
 pygame.init()
@@ -24,24 +25,30 @@ render.draw_full(array.values)
 # sort vai armazenar nossa funcao geradora
 sort = algorithms.bubble_sort(array.values)
 
+timer = 0
 running = True
+
 while running:   
+    dt = clock.tick(FPS) / 1000.0
+    timer += dt
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    while timer >= STEP:
+        try:
+            i, j = next(sort) # indices que foram trocados
             
-    try:
-        i, j = next(sort) # indices que foram trocados
+            array.swap(i, j)
+            render.update_bars(array.values, i, j) # atualiza apenas as barras trocadas
+            
+            pygame.display.flip()
+            
+        except StopIteration: # a funcao nao tem mais nenhum yield para retornar
+            running = False
         
-        array.swap(i, j)
-        render.update_bars(array.values, i, j) # atualiza apenas as barras trocadas
-        
-        pygame.display.flip()
-        clock.tick(FPS)
-        
-    except StopIteration: # a funcao nao tem mais nenhum yield para retornar
-        pygame.time.wait(2000)
-        running = False
+        timer -= STEP
         
 pygame.quit()
 sys.exit()
